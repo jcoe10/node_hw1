549f8b445de9c0444034d8bb07eaae4ed334f8e6
jcoe10 <jacob.e.coe@gmail.com>
2017-09-26 09:26:55 -0400

    added in spotify and it works

--- spotify_liri.js
+++ spotify_liri.js
@@ -1,40 +1,62 @@
-var spotify = require('spotify-web-api-node');
-
+//requiring all my packages
+var spotifyWebApi = require('node-spotify-api');
+var request = require('request');
 var fs = require('fs');
 
+//pulling the keys in from a separate and requiring them
 var spotKey = require('./spotify_keys.js');
+var spot = new spotifyWebApi(spotKey);
+//setting argument 2 in the terminal to "spotify this song "
+if (process.argv[2] == 'spotify-this-song' || 's') {
 
-if (process.argv[2] == 'spotify-this-song') {
-    var spot = new spotifyApi(spotKey)({
-        id: '3fc8e5bdbf2648b29915ce623d165209',
-        secret: 'e77709445f0b4610b2607d319c7c76c4'
-    });
-};
   
 
-        spotifyApi.clientCredentialsGrant()
 
-            .then(function (data) {
+// for loop to concatinate arguments
+var myInput = '';
 
-                spotifyApi.setAccessToken(data.body['access_token']);
-            }),
-            function (err) {
+if (process.argv.length > 2) {
+    for (var j = 3; j < process.argv.length; j++) {
+        myInput += (process.argv[j] + "+")
+    }
+}
+//creating a variable for the user so they can input a song
+        // var song = myInput;
 
-            };
+//part of the access token and authorization pulled from docs
 
-        var song = process.argv[3];
 
-        function spotifyIt(input) {
+spot.search({ type: 'track', query: myInput }, function(err, data) {
+ if (err) {
+   return console.log('Error occurred: ' + err);
+ }
 
-        }
-        spotify.search({
-            type: 'track',
-            query: 'All the Small Things'
-        }, function (err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err);
-            }
+console.log(data.tracks.items[0].name); 
+console.log(data.tracks.items[0].artists[0].external_urls.spotify);
+console.log(data.tracks.items[0].artists[0].name);
+console.log(data.tracks.items[0].album.name);
+});
+// function cases (argument, myInput) {
+// switch (argument) {
+//     case 'spotify-this-song':
+//         spotifyIt(song)
+//         break;
+// }
+// }
+};
 
-            console.log(data);
-        });
+//switch statement calling the argument
+// function cases (argument, myInput) {
+//     switch (argument) {
+//         //replaces if
+//         case "spotify-this-song":
+//         case 's':
+//             break;
+//         //replaces else
+//             console.log("Input Failed")
+    
+//     }
+// }
+// cases(process.argv[2]);
+        
         
--- twitter_liri.js
+++ twitter_liri.js
@@ -1,32 +1,35 @@
 //require twitter package
-var Twitter = require('twitter')
-
+var Twitter = require('twitter');
+//require twitter keys
 var twitKey = require('./twitter_keys.js');
 
-
+//argument variable
 var apis = process.argv;
 
 var client = new Twitter(twitKey);
-
+//user input for username and number of tweets pulled from their profile
 var params = {
     screen_name: 'jacobecoe',
     count: 20
 };
 
-if (process.argv[2] == 'my-tweets') {
+//setting argument 2 to my tweets or t and what the client gets with parameters
+if (process.argv[2] == 'my-tweets' || "t") {
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
+//if there is an error show it 
         if (error) {
             console.log(error);
         }
-
+//if no error print the tweets to the terminal
         if (!error) {
             for (var i = 0; i < 20; i++) {
                 console.log(tweets[i].text);
             }
         }
-
+//switch statement calling the argument 
         switch (twitKey) {
             case 'my-tweets':
+            case 't':
                 break;
         }
 
